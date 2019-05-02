1d70b7877227b529daa396deefb06ab6cabbd4d7
HGeorgiev75 <smart_mbox@abv.bg>
2019-05-02 11:08:19 +0300

    modify gitignore and PersonsDto

--- src/main/java/tests/dto/PersonsDto.java
+++ src/main/java/tests/dto/PersonsDto.java
@@ -3,6 +3,7 @@ package tests.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.io.Serializable;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PersonsDto implements Serializable {
@@ -10,6 +11,7 @@ public class PersonsDto implements Serializable {
 
     private Long id;
     private String name;
+    private List<PersonsDetailsDto> personsDetailsDtos;
 
     public Long getId() {
         return id;
@@ -25,5 +27,13 @@ public class PersonsDto implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<PersonsDetailsDto> getPersonsDetailsDtos() {
+        return personsDetailsDtos;
+    }
+
+    public void setPersonsDetailsDtos(List<PersonsDetailsDto> personsDetailsDtos) {
+        this.personsDetailsDtos = personsDetailsDtos;
     }
 }
